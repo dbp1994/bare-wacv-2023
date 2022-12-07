70a9dcf7a760a72ab85e945b139d9dcc4eaadb56
Deep Patel <40064864+dbp1994@users.noreply.github.com>
2022-12-07 21:42:02 +0530

    Add files via upload

---
+++ scripts/meta_layers.py
@@ -0,0 +1,451 @@
+from __future__ import print_function, absolute_import
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torch.optim import lr_scheduler
+from torch.autograd import Variable
+
+
+def to_var(x, requires_grad=True):
+    if torch.cuda.is_available():
+        x = x.cuda()
+    return x.requires_grad_(requires_grad)
+
+
+# Model
+
+class MetaModule(nn.Module):
+    # adopted from: Adrien Ecoffet https://github.com/AdrienLE
+
+    # The person who created the MetaModule or gave the idea for it:
+    # https://becominghuman.ai/paper-repro-learning-to-learn-by-gradient-descent-by-gradient-descent-6e504cc1c0de
+    # https://towardsdatascience.com/paper-repro-deep-metalearning-using-maml-and-reptile-fd1df1cc81b0
+
+    
+    def params(self):
+        for name, param in self.named_params(self):
+            yield param
+
+    def named_leaves(self):
+        return []
+
+    def named_submodules(self):
+        return []
+
+    def named_params(self, curr_module=None, memo=None, prefix=''):
+        if memo is None:
+            memo = set()
+
+        if hasattr(curr_module, 'named_leaves'):
+            for name, p in curr_module.named_leaves():
+                if p is not None and p not in memo:
+                    memo.add(p)
+                    yield prefix + ('.' if prefix else '') + name, p
+        else:
+            for name, p in curr_module._parameters.items():
+                if p is not None and p not in memo:
+                    memo.add(p)
+                    yield prefix + ('.' if prefix else '') + name, p
+
+        for mname, module in curr_module.named_children():
+            submodule_prefix = prefix + ('.' if prefix else '') + mname
+            for name, p in self.named_params(module, memo, submodule_prefix):
+                yield name, p
+
+    def update_params(self, lr_inner, first_order=False, source_params=None, detach=False):
+        if source_params is not None:
+            for tgt, src in zip(self.named_params(self), source_params):
+                name_t, param_t = tgt
+                # name_s, param_s = src
+                # grad = param_s.grad
+                # name_s, param_s = src
+                grad = src
+                if first_order:
+                    grad = to_var(grad.detach().data)
+                tmp = param_t - lr_inner * grad
+                self.set_param(self, name_t, tmp)
+        else:
+
+            for name, param in self.named_params(self):
+                if not detach:
+                    grad = param.grad
+                    if first_order:
+                        grad = to_var(grad.detach().data)
+                    tmp = param - lr_inner * grad
+                    self.set_param(self, name, tmp)
+                else:
+                    param = param.detach_()
+                    self.set_param(self, name, param)
+
+    def set_param(self, curr_mod, name, param):
+        if '.' in name:
+            n = name.split('.')
+            module_name = n[0]
+            rest = '.'.join(n[1:])
+            for name, mod in curr_mod.named_children():
+                if module_name == name:
+                    self.set_param(mod, rest, param)
+                    break
+        else:
+            setattr(curr_mod, name, param)
+            # print("\n curr_mod: {}, name: {}\n".format(curr_mod, name))
+
+    def detach_params(self):
+        for name, param in self.named_params(self):
+            self.set_param(self, name, param.detach())
+
+    def copy(self, other, same_var=False):
+        for name, param in other.named_params():
+            if not same_var:
+                param = to_var(param.data.clone(), requires_grad=True)
+            self.set_param(name, param)
+
+
+class MetaLinear(MetaModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        ignore = nn.Linear(*args, **kwargs)
+       
+        self.register_buffer('weight', to_var(ignore.weight.data, requires_grad=True))
+        self.register_buffer('bias', to_var(ignore.bias.data, requires_grad=True))
+        
+    def forward(self, x):
+        return F.linear(x, self.weight, self.bias)
+    
+    def named_leaves(self):
+        return [('weight', self.weight), ('bias', self.bias)]
+    
+class MetaConv2d(MetaModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        ignore = nn.Conv2d(*args, **kwargs)
+        
+        self.stride = ignore.stride
+        self.padding = ignore.padding
+        self.dilation = ignore.dilation
+        self.groups = ignore.groups
+        
+        self.register_buffer('weight', to_var(ignore.weight.data, requires_grad=True))
+        
+        if ignore.bias is not None:
+            self.register_buffer('bias', to_var(ignore.bias.data, requires_grad=True))
+        else:
+            self.register_buffer('bias', None)
+        
+    def forward(self, x):
+        return F.conv2d(x, self.weight, self.bias, self.stride, self.padding, self.dilation, self.groups)
+    
+    def named_leaves(self):
+        return [('weight', self.weight), ('bias', self.bias)]
+    
+class MetaConvTranspose2d(MetaModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        ignore = nn.ConvTranspose2d(*args, **kwargs)
+        
+        self.stride = ignore.stride
+        self.padding = ignore.padding
+        self.dilation = ignore.dilation
+        self.groups = ignore.groups
+        
+        self.register_buffer('weight', to_var(ignore.weight.data, requires_grad=True))
+        
+        if ignore.bias is not None:
+            self.register_buffer('bias', to_var(ignore.bias.data, requires_grad=True))
+        else:
+            self.register_buffer('bias', None)
+        
+    def forward(self, x, output_size=None):
+        output_padding = self._output_padding(x, output_size)
+        return F.conv_transpose2d(x, self.weight, self.bias, self.stride, self.padding,
+            output_padding, self.groups, self.dilation)
+       
+    def named_leaves(self):
+        return [('weight', self.weight), ('bias', self.bias)]
+
+
+class MetaBatchNorm1d(MetaModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        ignore = nn.BatchNorm1d(*args, **kwargs)
+        
+        self.num_features = ignore.num_features
+        self.eps = ignore.eps
+        self.momentum = ignore.momentum
+        self.affine = ignore.affine
+        self.track_running_stats = ignore.track_running_stats
+
+        if self.affine:           
+            self.register_buffer('weight', to_var(ignore.weight.data, requires_grad=True))
+            self.register_buffer('bias', to_var(ignore.bias.data, requires_grad=True))
+            
+        if self.track_running_stats:
+            self.register_buffer('running_mean', torch.zeros(self.num_features))
+            self.register_buffer('running_var', torch.ones(self.num_features))
+        else:
+            self.register_parameter('running_mean', None)
+            self.register_parameter('running_var', None)
+
+        
+    def forward(self, x):
+        return F.batch_norm(x, self.running_mean, self.running_var, self.weight, self.bias,
+                        self.training or not self.track_running_stats, self.momentum, self.eps)
+            
+    def named_leaves(self):
+        return [('weight', self.weight), ('bias', self.bias)]
+
+
+class MetaBatchNorm2d(MetaModule):
+    def __init__(self, *args, **kwargs):
+        super().__init__()
+        ignore = nn.BatchNorm2d(*args, **kwargs)
+        
+        self.num_features = ignore.num_features
+        self.eps = ignore.eps
+        self.momentum = ignore.momentum
+        self.affine = ignore.affine
+        self.track_running_stats = ignore.track_running_stats
+
+        if self.affine:           
+            self.register_buffer('weight', to_var(ignore.weight.data, requires_grad=True))
+            self.register_buffer('bias', to_var(ignore.bias.data, requires_grad=True))
+            
+        if self.track_running_stats:
+            self.register_buffer('running_mean', torch.zeros(self.num_features))
+            self.register_buffer('running_var', torch.ones(self.num_features))
+        else:
+            self.register_parameter('running_mean', None)
+            self.register_parameter('running_var', None)
+
+        
+    def forward(self, x):
+        return F.batch_norm(x, self.running_mean, self.running_var, self.weight, self.bias,
+                        self.training or not self.track_running_stats, self.momentum, self.eps)
+            
+    def named_leaves(self):
+        return [('weight', self.weight), ('bias', self.bias)]
+
+
+class LeNet(MetaModule):
+    def __init__(self, n_out):
+        super(LeNet, self).__init__()
+    
+        layers = []
+        layers.append(MetaConv2d(1, 6, kernel_size=5))
+        layers.append(nn.ReLU(inplace=True))
+        layers.append(nn.MaxPool2d(kernel_size=2,stride=2))
+
+        layers.append(MetaConv2d(6, 16, kernel_size=5))
+        layers.append(nn.ReLU(inplace=True))
+        layers.append(nn.MaxPool2d(kernel_size=2,stride=2))
+        
+        layers.append(MetaConv2d(16, 120, kernel_size=5))
+        layers.append(nn.ReLU(inplace=True))
+        
+        self.main = nn.Sequential(*layers)
+        
+        layers = []
+        layers.append(MetaLinear(120, 84))
+        layers.append(nn.ReLU(inplace=True))
+        layers.append(MetaLinear(84, n_out))
+        
+        self.fc_layers = nn.Sequential(*layers)
+        
+    def forward(self, x):
+        x = self.main(x)
+        x = x.view(-1, 120)
+        return self.fc_layers(x).squeeze()
+
+
+class MNIST_MetaNN(MetaModule):
+    def __init__(self):
+        super(MNIST_MetaNN, self).__init__()
+
+        # 1 I/P channel, 6 O/P channels, 5x5 conv. kernel
+        self.conv1 = MetaConv2d(in_channels=1, out_channels=6, kernel_size=5, padding=2)
+        self.conv2 = MetaConv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.conv3 = MetaConv2d(in_channels=16, out_channels=120, kernel_size=5)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+
+        # self.fc1 = nn.Linear(400, 120)
+        self.fc1 = MetaLinear(120, 84)
+        self.fc2 = MetaLinear(84, 10)
+    
+    def forward(self, x):
+
+        # x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2), stride=2)
+        # x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2), stride=2)
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:] # all dims except batch_size dim
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
+class CIFAR_MetaNN(MetaModule):
+    def __init__(self, temp=1.0):
+        super(CIFAR_MetaNN, self).__init__()
+
+        # 1 I/P channel, 6 O/P channels, 5x5 conv. kernel
+        self.conv1 = MetaConv2d(in_channels=3, out_channels=6, kernel_size=5, padding=2)
+        self.conv2 = MetaConv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.conv3 = MetaConv2d(in_channels=16, out_channels=120, kernel_size=5)
+
+        self.fc1 = MetaLinear(480, 120)
+        self.fc2 = MetaLinear(120, 84)
+        self.fc3 = MetaLinear(84, 10)
+
+        self.temp = temp
+    
+    def forward(self, x):
+
+        # x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2), stride=2)
+        # x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2), stride=2)
+        x = F.max_pool2d(F.relu(self.conv1(x)), kernel_size=2, stride=2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), kernel_size=2, stride=2)
+        x = F.relu(self.conv3(x))
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = F.relu(self.fc3(x)) / self.temp
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:] # all dims except batch_size dim
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+class Meta_CNN_small(MetaModule):
+    def __init__(self, num_classes=10):
+        super(Meta_CNN_small, self).__init__()
+        self.conv1 = MetaConv2d(3, 6, 5)
+        self.conv2 = MetaConv2d(6, 16, 5)
+        self.fc1 = MetaLinear(16 * 5 * 5, 120)
+        self.fc2 = MetaLinear(120, 84)
+        self.fc3 = MetaLinear(84, num_classes)
+
+    def forward(self, x):
+        x = F.max_pool2d(F.relu(self.conv1(x)), kernel_size=2, stride=2)
+        x = F.max_pool2d(F.relu(self.conv2(x)), kernel_size=2, stride=2)
+        x = x.view(-1, 16 * 5 * 5)
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return x
+
+
+def call_bn(bn, x):
+    return bn(x)
+
+class Meta_CNN(MetaModule):
+    def __init__(self, input_channel=3, n_outputs=10, dropout_rate=0.25, momentum=0.1):
+        self.dropout_rate = dropout_rate
+        self.momentum = momentum 
+        super(Meta_CNN, self).__init__()
+        self.c1=MetaConv2d(input_channel, 64,kernel_size=3,stride=1, padding=1)        
+        # self.c2=MetaConv2d(64,64,kernel_size=3,stride=1, padding=1)        
+        self.c3=MetaConv2d(64,128,kernel_size=3,stride=1, padding=1)        
+        # self.c4=MetaConv2d(128,128,kernel_size=3,stride=1, padding=1)        
+        self.c5=MetaConv2d(128,196,kernel_size=3,stride=1, padding=1)        
+        self.c6=MetaConv2d(196,16,kernel_size=3,stride=1, padding=1)        
+        self.linear1=MetaLinear(256, n_outputs)
+        self.bn1=MetaBatchNorm2d(64, momentum=self.momentum)
+        # self.bn2=MetaBatchNorm2d(64, momentum=self.momentum)
+        self.bn3=MetaBatchNorm2d(128, momentum=self.momentum)
+        # self.bn4=MetaBatchNorm2d(128, momentum=self.momentum)
+        self.bn5=MetaBatchNorm2d(196, momentum=self.momentum)
+        self.bn6=MetaBatchNorm2d(16, momentum=self.momentum)
+
+    def forward(self, x,):
+        h=x
+        h=self.c1(h)
+        h=F.relu(call_bn(self.bn1, h))
+        # h=self.c2(h)
+        # h=F.relu(call_bn(self.bn2, h))
+        h=F.max_pool2d(h, kernel_size=2, stride=2)
+
+        h=self.c3(h)
+        h=F.relu(call_bn(self.bn3, h))
+        # h=self.c4(h)
+        # h=F.relu(call_bn(self.bn4, h))
+        h=F.max_pool2d(h, kernel_size=2, stride=2)
+
+        h=self.c5(h)
+        h=F.relu(call_bn(self.bn5, h))
+        h=self.c6(h)
+        h=F.relu(call_bn(self.bn6, h))
+        h=F.max_pool2d(h, kernel_size=2, stride=2)
+
+        h = h.view(h.size(0), -1)
+        logit=self.linear1(h)
+        return logit
+
+class MLPNet(MetaModule):
+    def __init__(self):
+        super(MLPNet, self).__init__()
+        self.fc1 = MetaLinear(28*28, 256)
+        self.fc2 = MetaLinear(256, 10)
+
+    def forward(self, x):
+        x = x.view(-1, 28*28)
+        x = F.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+
+# Define Model
+class MNIST_NN(nn.Module):
+    def __init__(self):
+        super(MNIST_NN, self).__init__()
+
+        # 1 I/P channel, 6 O/P channels, 5x5 conv. kernel
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=6, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv2d(in_channels=6, out_channels=16, kernel_size=5)
+        self.conv3 = nn.Conv2d(in_channels=16, out_channels=120, kernel_size=5)
+
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.fc1 = nn.Linear(120, 84)
+        self.fc2 = nn.Linear(84, 10)
+    
+    def forward(self, x):
+
+        # x = F.max_pool2d(F.relu(self.conv1(x)), (2, 2), stride=2)
+        # x = F.max_pool2d(F.relu(self.conv2(x)), (2, 2), stride=2)
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = F.relu(self.conv3(x))
+        x = x.view(-1, self.num_flat_features(x))
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+
+        return x
+
+    def num_flat_features(self, x):
+        size = x.size()[1:] # all dims except batch_size dim
+        num_features = 1
+        for s in size:
+            num_features *= s
+        return num_features
+
+
+# If one wants to freeze layers of a network
+# for param in model.parameters():
+#   param.requires_grad = False
