2dfe715f5ffa15eb0dc5b38db114a72ea76700a5
Deep Patel <40064864+dbp1994@users.noreply.github.com>
2022-12-07 17:24:45 +0530

    Add files via upload

---
+++ scripts/add_noise.py
@@ -0,0 +1,165 @@
+import numpy as np
+from numpy.testing import assert_array_almost_equal
+import torch
+
+# set seed for reproducibility
+np.random.seed(3459)
+
+
+def build_uniform_P(size, noise):
+    """ The noise matrix flips any class to any other with probability
+    noise / (#class - 1).
+    """
+    assert(noise >= 0.) and (noise <= 1.)
+
+    P = noise / (size - 1) * np.ones((size, size))
+    np.fill_diagonal(P, (1 - noise) * np.ones(size))
+
+    assert_array_almost_equal(P.sum(axis=1), 1, 1)
+    return P
+
+def noisify_with_P(y_train, nb_classes, noise, random_state=None):
+
+    if noise > 0.0:
+        P = build_uniform_P(nb_classes, noise)
+        # seed the random numbers with #run
+        y_train_noisy = multiclass_noisify(y_train, P=P,
+                                           random_state=random_state)
+        actual_noise = (y_train_noisy != y_train).mean()
+        assert actual_noise > 0.0
+        print('Actual noise %.2f' % actual_noise)
+        y_train = y_train_noisy
+    else:
+        P = np.eye(nb_classes)
+
+    return y_train, P
+
+def multiclass_noisify(y, P, random_state=0):
+    """ Flip classes according to transition probability matrix T.
+    It expects a number between 0 and the number of classes - 1.
+    """
+
+    assert P.shape[0] == P.shape[1]
+    assert np.max(y) < P.shape[0]
+
+    # row stochastic matrix
+    assert_array_almost_equal(P.sum(axis=1), np.ones(P.shape[1]))
+    assert (P >= 0.0).all()
+
+    m = y.shape[0]
+    new_y = y.copy()
+    flipper = np.random.RandomState(random_state)
+
+    for idx in np.arange(m):
+        i = y[idx]
+        # draw a vector with only an 1
+        flipped = flipper.multinomial(1, P[int(i), :], 1)[0]
+        new_y[idx] = np.where(flipped == 1)[0]
+
+    return new_y
+
+def noisify_mnist_asymmetric(y_train, noise, random_state=None):
+    """mistakes:
+        1 <- 7
+        2 -> 7
+        3 -> 8
+        5 <-> 6
+    """
+    nb_classes = 10
+    P = np.eye(nb_classes)
+    n = noise
+
+    if n > 0.0:
+        # 1 <- 7
+        P[7, 7], P[7, 1] = 1. - n, n
+
+        # 2 -> 7
+        P[2, 2], P[2, 7] = 1. - n, n
+
+        # 5 <-> 6
+        P[5, 5], P[5, 6] = 1. - n, n
+        P[6, 6], P[6, 5] = 1. - n, n
+
+        # 3 -> 8
+        P[3, 3], P[3, 8] = 1. - n, n
+
+        y_train_noisy = multiclass_noisify(y_train, P=P,
+                                           random_state=random_state)
+        actual_noise = (y_train_noisy != y_train).mean()
+        assert actual_noise > 0.0
+        print('Actual noise %.2f' % actual_noise)
+        y_train = y_train_noisy
+
+    return y_train, P
+
+def noisify_news_asymmetric(y_train, noise, random_state=None):
+
+    """mistakes:
+        0 - religion <-> 5 - politics
+        1 - computers -> 4 - science
+        2 - for sale <- 3 - autos/motorcylce
+    """
+
+    nb_classes = 6
+    P = np.eye(nb_classes)
+    n = noise
+
+    if n > 0.0:
+        # religion <-> politics
+        P[0, 0], P[0, 5] = 1. -n, n
+        P[5, 0], P[5, 5] = n, 1. - n
+
+        # computers -> science
+        P[1, 1], P[1, 4] = 1. - n, n
+
+        # for sale <- autos/motorcylce
+        P[3, 3], P[3, 2] = 1. - n, n
+
+        y_train_noisy = multiclass_noisify(y_train, P=P,
+                                           random_state=random_state)
+        actual_noise = (y_train_noisy != y_train).mean()
+        assert actual_noise > 0.0
+        print('Actual noise %.2f' % actual_noise)
+        y_train = y_train_noisy
+
+        return y_train, P
+
+def noisify_cifar10_asymmetric(y_train, noise, random_state=None):
+    """mistakes:
+        automobile <- truck
+        bird -> airplane
+        cat <-> dog
+        deer -> horse
+    """
+    nb_classes = 10
+    P = np.eye(nb_classes)
+    n = noise
+
+    if n > 0.0:
+        # automobile <- truck
+        P[9, 9], P[9, 1] = 1. - n, n
+
+        # bird -> airplane
+        P[2, 2], P[2, 0] = 1. - n, n
+
+        # cat <-> dog
+        P[3, 3], P[3, 5] = 1. - n, n
+        P[5, 5], P[5, 3] = 1. - n, n
+
+        # automobile -> truck
+        P[4, 4], P[4, 7] = 1. - n, n
+
+        y_train_noisy = multiclass_noisify(y_train, P=P,
+                                           random_state=random_state)
+        actual_noise = (y_train_noisy != y_train).mean()
+        assert actual_noise > 0.0
+        print('Actual noise %.2f' % actual_noise)
+        y_train = y_train_noisy
+
+    return y_train, P
+
+def norm(T):
+    row_abs = torch.abs(T)
+    row_sum = torch.sum(row_abs, 1).unsqueeze(1)
+    T_norm = row_abs / row_sum
+    return T_norm
